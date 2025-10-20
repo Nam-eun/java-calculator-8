df8919c99a6814956981b708176be3eb7c8855d3
Nam-eun <123121510+Nam-eun@users.noreply.github.com>
2025-10-20 22:04:02 +0900

    message

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -17,8 +19,26 @@ public class Application {
 
     public static int Change(String input){
 
-        System.out.println(input);
-        return 0;
+        String[] arr = {};
+        String[] arr_pre = {};
 
+        if(input.indexOf("//") != 0 ){
+
+            arr = input.split(",|:");
+
+
+        }else{
+
+            String my_ctm = input.substring(input.indexOf("//")+2,input.indexOf("\\n"));
+
+            arr_pre = input.split(Pattern.quote("\\n"));
+
+            for(int i = 0; i<arr_pre.length; i++){
+
+                arr = arr_pre[i].split(Pattern.quote(my_ctm));
+            }
+        }
+
+        return 0;
     }
 }
