52796406baea118ab8982e189f1335d72442515f
Nam-eun <123121510+Nam-eun@users.noreply.github.com>
2025-10-20 21:38:19 +0900

    feat: 사용자에게 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,22 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = readLine();
+
+        int result = Change(input);
+
+        System.out.println("결과 : " + result);
+    }
+
+    public static int Change(String input){
+
+        System.out.println(input);
+        return 0;
+
     }
 }
